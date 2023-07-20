37fa099d439ca71bb32139f4b341bfdef84037e8
Mary Josephine Gamit <josephinegamit13@gmail.com>
2023-07-20 13:56:43 +0800

    Updated version

--- predict.py
+++ predict.py
@@ -1,1 +1,5 @@
 from ultralytics import YOLO
+
+model = YOLO('best_so_far.pt')
+
+model.predict(source=['0.jpg', '01.jpg', '02.jpg', '03.jpg', '04.jpg'], show=True, save=True, show_labels=True, conf=0.5, save_txt=False, save_crop=False, line_width=2, box=True, visualize=False)
